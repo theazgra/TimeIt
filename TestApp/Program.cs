1b74dbd2143a5875c3e67e35bbcbaa1d34a67863
Vojtěch Moravec <theazgra@gmail.com>
2020-01-27 19:03:13 +0100

    Test application.

---
+++ TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (true)
+            {
+                Console.WriteLine("TestApp output");
+                if ((++counter % 5) == 0)
+                {
+                    Console.Error.WriteLine("TestApp error output.");
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ TimeIt/ProcessTimes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TimeIt
+{
+    struct ProcessTimes
+    {
+        /// <summary>
+        /// Total wall time of the measured process.
+        /// </summary>
+        internal TimeSpan wallTime;
+
+        /// <summary>
+        /// Process time spent in kernel mode.
+        /// </summary>
+        internal TimeSpan kernelTime;
+
+        /// <summary>
+        /// Process time spent in user mode.
+        /// </summary>
+        internal TimeSpan userTime;
+    }
+}
