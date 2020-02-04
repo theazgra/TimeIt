e80b995bf53381ca4e58c3e7bf338f93f2f84763
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2020-02-04 09:20:49 +0100

    Copy test app to TimeIt binary folder.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,17 +7,18 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+
             int counter = 0;
             while (true)
             {
                 Console.WriteLine("TestApp output");
+                Thread.Sleep(args.Length > 0 ? int.Parse(args[0]) : 200);
                 if ((++counter % 2) == 0)
                 {
                     Console.Error.WriteLine("TestApp error output.");
 
                     break;
                 }
-                Thread.Sleep(1000);
             }
         }
     }
